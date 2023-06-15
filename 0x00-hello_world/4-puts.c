412333411b862a2dd01b24fa2a91d0e131ed4cf6
Fahem Ahmed <fahemfahem26@gmail.com>
2023-06-15 11:54:47 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+
+int main() {
+    const char *message = "\"Programming is like building a multilingual puzzle";
+    puts(message);
+
+    return 0;
 }
+
