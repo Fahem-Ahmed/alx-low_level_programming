7a5d621b86acb2ab1c091491ab0094f37145ecd3
Fahem Ahmed <fahemfahem26@gmail.com>
2023-07-07 14:53:00 -0700

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,10 +2,8 @@
 /**
  * main - Write a program that prints all arguments it receives
  *All arguments should be printed, including the first on
- *
  * @argc: This is the argument count 
  * @argv: This is the argument vector
- *
  * Return: 0;
  */
 int main(int argc, char *argv[])
